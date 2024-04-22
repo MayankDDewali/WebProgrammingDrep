0d27b0685c0606c231e8ea0186ee258470f47e63
MayankDDewali <mayankddewali@gmail.com>
2024-04-23 02:41:24 +0530

    adding gallery page

--- ProjectsD/CodingAcademyD/js/functions.js
+++ ProjectsD/CodingAcademyD/js/functions.js
@@ -1,9 +1,24 @@
 var nav = document.querySelector('nav');
 
-      window.addEventListener('scroll', function () {
-        if (window.pageYOffset > 500) {
-          nav.classList.add('bg-dark', 'shadow');
-        } else {
-          nav.classList.remove('bg-dark', 'shadow');
-        }
-      });
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 500) {
+    nav.classList.add('bg-dark', 'shadow');
+  } else {
+    nav.classList.remove('bg-dark', 'shadow');
+  }
+});
+
+
+/*                                    Gallery Page Slider functions                                   */
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
